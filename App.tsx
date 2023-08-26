1f98a8a7a4d0ae653930253a2d034117d992f875
Holly Canham <hollycanham@Hollys-MBP.broadband>
2023-08-26 10:48:42 +0100

    feat: basic tabs

--- App.tsx
+++ App.tsx
@@ -1,8 +1,51 @@
-import React from 'react';
-import {Text} from 'react-native';
+import * as React from 'react';
+import {Text, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-function App(): JSX.Element {
-  return <Text>Home</Text>;
+function TeamScreen() {
+  return (
+    <View>
+      <Text>Team!</Text>
+    </View>
+  );
 }
 
-export default App;
+function LeaguesScreen() {
+  return (
+    <View>
+      <Text>Leagues!</Text>
+    </View>
+  );
+}
+
+function FixturesScreen() {
+  return (
+    <View>
+      <Text>Fixtures!</Text>
+    </View>
+  );
+}
+
+function StatsScreen() {
+  return (
+    <View>
+      <Text>Stats!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Team" component={TeamScreen} />
+        <Tab.Screen name="Leagues" component={LeaguesScreen} />
+        <Tab.Screen name="Fixtures" component={FixturesScreen} />
+        <Tab.Screen name="Stats" component={StatsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
